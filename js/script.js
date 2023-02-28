b5481bc2b867ee9f7568ed4be5f7eb72023a323b
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-02-28 15:29:57 +0100

    doc: added html js and css file and folders, started first exercise notes

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+/*
+TRACK:
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+/*
+pseud-code:
+-creazione lista email
+-selezionare email da un input
+    con uso di un bottone
+-SE l'email è presente
+    Verificata
+-SE l'ipotesi precende è vera
+    stampare email: found output
+ALTRIMENTI
+    stampare email: not-found output
+*/
+
+
+
